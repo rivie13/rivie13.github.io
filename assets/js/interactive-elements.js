3d1f8f66e014c9e053f41ed36781dc5f17f8e852
Riviera Sperduto <riviera.t.sperduto13@gmail.com>
2025-05-25 21:28:11 -0400

    Add environment variables for API keys in _config.yml, update Gemfile to include dotenv, and create new config.yml for endpoint management. Refactor interactive-elements.js to use new endpoint structure.

--- assets/js/interactive-elements.js
+++ assets/js/interactive-elements.js
@@ -1,9 +1,9 @@
 const ENDPOINTS = {
-  newChat: "https://blogpost-interactive-elements.azurewebsites.net/api/NewMethodProxy?code=REMOVED ",
-  newSnippet: "https://blogpost-interactive-elements.azurewebsites.net/api/NewMethodProxy/tower-snippet?code=REMOVED ",
-  oldChat: "https://blogpost-interactive-elements.azurewebsites.net/api/OldMethodProxy?code=REMOVED ",
-  oldSnippet: "https://blogpost-interactive-elements.azurewebsites.net/api/OldMethodProxy/tower-snippet?code=REMOVED ",
-  executeTwoSum: "https://blogpost-interactive-elements.azurewebsites.net/api/ExecuteTwoSumSolutionProxy?code=REMOVED "
+  newChat: "{{ site.data.config.endpoints.newChat }}",
+  newSnippet: "{{ site.data.config.endpoints.newSnippet }}",
+  oldChat: "{{ site.data.config.endpoints.oldChat }}",
+  oldSnippet: "{{ site.data.config.endpoints.oldSnippet }}",
+  executeTwoSum: "{{ site.data.config.endpoints.executeTwoSum }}"
 };
 
 class InteractiveElements {
